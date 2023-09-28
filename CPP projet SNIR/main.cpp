f3b8a09afd270230cb5402ff4a186c9f7915f39d
GeoffroyTunez <tunezgeoffroy@gmail.com>
2023-09-28 18:56:51 +0200

    commit avec date auto pour nom du fichier txt

--- CPP projet SNIR/main.cpp
+++ CPP projet SNIR/main.cpp
@@ -5,52 +5,52 @@
 #include "fonctions.h"
 using namespace std;
 
-
-
-// compiler le programme en tapent : g++ -o main main.cpp fonctions.cpp
+// compiler le programme en tapant : g++ -o main main.cpp fonctions.cpp
 // lancer le programme en tapant : ./main 
-
 
 int main() {
     srand(time(NULL)); // Initialisation du générateur de nombres aléatoires
     int choixUtilisateur;
     int choixOrdinateur;
     string nomUtilisateur;
-    int score =0 ;
+    int score = 0;
     int nbrparti = 0;
-    int i = 0;
     
     cout << "Quelle est votre prenom ? : ";
     cin >> nomUtilisateur;
 
-    cout << "Combien de parti voulez vous jouer ? : ";
+    cout << "Combien de parties voulez-vous jouer ? : ";
     cin >> nbrparti;
 
-    cout << " Vous voulez faire  : " << nbrparti << " tours" << endl;  
+    cout << "Vous voulez faire : " << nbrparti << " tours" << endl;  
 
-
-    for( i=0 ; i<nbrparti ; i++){
-
+    for (int i = 0; i < nbrparti; i++) {
         // Demander à l'utilisateur de choisir entre
         cout << "Choisissez : pierre (1), papier (2) ou ciseaux (3) : ";
-        cin>> choixUtilisateur;
+        cin >> choixUtilisateur;
 
         // Appeler la fonction pour générer le choix de l'ordinateur
         choixOrdinateur = genererChoixOrdinateur();
-        cout << "l'ordinateur a choisi : " <<  choixOrdinateur << endl; 
+        cout << "L'ordinateur a choisi : " << choixOrdinateur << endl; 
 
         // Appeler la fonction pour déterminer le gagnant
         int resultat = determinerGagnant(choixUtilisateur, choixOrdinateur);
         score = afficherResultat(score, resultat);
-
     }
     cout << "Votre score est : " << score << endl;
 
-    string const nomdufichier ( "C:/Users/Geoffroy/Desktop/git geo/CPP projet SNIR/file.txt" );
-    ofstream monflux (nomdufichier.c_str()); 
-    if( monflux) {
-       monflux << "Nom de l'utilisateur : " << nomUtilisateur << endl << "Score de l'utilisateur : " << score << endl;
+    // Obtenir la date actuelle
+    time_t now = time(0);
+    tm* localTime = localtime(&now);
+    char dateStr[80];
+    strftime(dateStr, sizeof(dateStr), "%Y-%m-%d", localTime);
 
+    // Créer le nom de fichier avec la date d'aujourd'hui
+    string nomFichier = "C:/Users/Geoffroy/Desktop/git geo/CPP projet SNIR/" + string(dateStr) + "_file.txt";
+
+    ofstream monflux(nomFichier.c_str()); 
+    if (monflux) {
+       monflux << "Nom de l'utilisateur : " << nomUtilisateur << endl << "Score de l'utilisateur : " << score << endl;
     }
     return 0;
 }
